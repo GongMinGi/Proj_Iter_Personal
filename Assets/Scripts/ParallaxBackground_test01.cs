9f5032939fd6ff9d8ba7b2fb6fdb979ad014ec87
GongMinGi <komki@naver.com>
2024-12-16 20:36:06 +0900

    주인공 jump, 이동 구현 완료, parallax background 구현중

---
+++ Assets/Scripts/ParallaxBackground_test01.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class ParallaxBackground_test01 : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform target;   // 현재 배경과 이어지는 배경
+    [SerializeField]
+    private float scrollAmount; // 이어지는 두 배경 사이의 거리
+    [SerializeField]
+    private float scrollSpeed; // 이동속도
+    [SerializeField]
+    private Vector3 moveDirection; // 이동 방향
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //배경이 moveDirection 방향으로 moveSpeed 속도로 이동
+        transform.position += moveDirection * scrollSpeed * Time.deltaTime;
+
+        //배경이 설정된 범위를 벗어나면 위치 재설정
+        if(transform.position.x <= -scrollAmount)
+        {
+            transform.position = target.position - moveDirection * scrollAmount;
+        }
+        
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.InputSystem.Composites;
 
 // made by mingi
 
@@ -13,19 +14,28 @@ public class PlayerController : MonoBehaviour
     bool isGround = true;
     Rigidbody2D playerRigid;
     Transform playerDirection;
-    Animator playerAnimator;
+    Animator playerAnim;
+    SpriteRenderer spriteRenderer;
 
-    void Start()
+
+    void Awake()
     {
         playerRigid = GetComponent<Rigidbody2D>();
-        playerAnimator = GetComponent<Animator>();
+        playerAnim = GetComponent<Animator>();
         playerDirection = GetComponent<Transform>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Jump();
+
+        //sprite를 뒤집는 코드. GetAxisRaw(horizontal)은 a를 누를땐 -1, d를 누를 땐 1이므로 
+        //a를 누르면 -1과 같아져 true, d를 누르면 -1과 달라져서 false를 반환하게 됨.
+        // 또한 getbuttondown은 버튼이 처음으로 눌렸을대만 true를 반환한다.
+        if (Input.GetButton("Horizontal"))
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
         
     }
 
@@ -34,14 +44,32 @@ public class PlayerController : MonoBehaviour
         float moveInput = Input.GetAxisRaw("Horizontal");
         playerRigid.linearVelocity = new Vector2(moveInput * moveSpeed, playerRigid.linearVelocity.y);
 
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        //landing platform
+        if(playerRigid.linearVelocity.y < 0 ) //추락할때만 레이를 아래로 쏜다.
         {
-            playerRigid.linearVelocity = new Vector2(playerRigid.linearVelocity.y,jumpForce);
+            Debug.DrawRay(playerRigid.position, Vector3.down, new Color(0, 1, 0));
+            RaycastHit2D rayHit = Physics2D.Raycast(playerRigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+            if (rayHit.collider != null)
+            {
+                Debug.Log(rayHit.collider.name);
+
+                if (rayHit.distance < 1f)
+                {
+                    Debug.Log(rayHit.collider.name);
+                    playerAnim.SetBool("isJumping", false);
+                }
+            }
         }
+
     }
 
     void Jump()
     {
-
+        if (Input.GetKeyDown(KeyCode.Space) && !playerAnim.GetBool("isJumping"))
+        {
+            playerRigid.linearVelocity = new Vector2(playerRigid.linearVelocity.y, jumpForce);
+            playerAnim.SetBool("isJumping", true);
+        }
     }
 }
