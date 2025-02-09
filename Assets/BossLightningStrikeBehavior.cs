84d4d81ff847bdb7d412fbec13fd53c3757465f7
GongMinGi <komki@naver.com>
2025-02-10 03:10:33 +0900

    번개 경고선 구현, 파티클 z축 으로출력돼는 오류 해결, 번개 오브젝트 컬링 진행중

--- Assets/BossLightningStrikeBehavior.cs
+++ Assets/BossLightningStrikeBehavior.cs
@@ -9,50 +9,96 @@ public class BossLightningStrikeBehavior : StateMachineBehaviour
     public float maxTime;
     public float LightningDelay;
     public GameObject lightningStrike;
-
+    private GameObject lightningObject; //오브젝트 컬링을 위해 프리팹을 담을 오브젝트 변수
     [SerializeField]
     private float lightningHeight; // 번개가 떨어지는 높이
+    
 
+
+    [SerializeField]
+    private float lightningTimer; // 실제로 업데이트에서 쓰일 딜레이변수, OnStateEnter에서 lightningDelay로 초기화해줘야함
+    private LineRenderer lineRenderer; // 번개 경고를 위한 라인 랜더러 가져오기
     private Vector2 playerpos; // 플레이어의 위치
     private Vector2 originBossPos; // 보스의 위치
-    private Vector2 LightningStartPos; //번개가 떨어지기 시작하는 위치
+    private Vector2 lightningStartPos; //번개가 떨어지기 시작하는 위치
+    private Vector2 lightningEndPos;
+
+    private bool canAttack;
 
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        Debug.Log("Lightning Strike");
-        // 플레이어의 위에서 아래로 1초정도 레이케스트로 경고 
-        // 시간이 지나면, 번개프리팹 생성
-        GameObject boss = animator.gameObject;
-        originBossPos = animator.transform.position;
-        //태그를 통해 플레이어의 위치벡터 구하기
-        playerpos = GameObject.FindGameObjectWithTag("Player").transform.position;
-        LightningStartPos = new Vector2(playerpos.x, originBossPos.y + lightningHeight); // 플레이어의 머리 위 조금 떨어진 지점에서 번개 출력
+        timer = Random.Range(minTime, maxTime);
+        lightningTimer = LightningDelay;// 공격 딜레이 초기화
 
-       
+
+        GameObject boss = animator.gameObject; // 컴포넌트에 접속하기 위해 보스 오브젝트 변수로 가져오기
+        lineRenderer = boss.GetComponent<LineRenderer>(); //라인랜더러 할당하기
+
+        originBossPos = animator.transform.position;
+        playerpos = GameObject.FindGameObjectWithTag("Player").transform.position;  //태그를 통해 플레이어의 위치벡터 구하기
+        lightningStartPos = new Vector2(playerpos.x, originBossPos.y + lightningHeight); // 플레이어의 머리 위 조금 떨어진 지점에서 번개 출력
+        lightningEndPos = new Vector2(lightningStartPos.x, lightningStartPos.y - lightningHeight);
+
+
+        //첫 실행 시에 한번 프리팹을 넣어주고 비활성화시켜놓는다.
+        if(lightningObject == null)
+        {
+            lightningObject = Instantiate(lightningStrike, lightningStartPos, Quaternion.Euler(90, 0, 0));
+            lightningObject.SetActive(false);
+        }
+
+
     }
 
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        Debug.Log(LightningDelay);
+        Debug.Log(lightningTimer);
         //라이트닝 딜레이가 남아있을 때만 레이케스트를 쏜다.
-        if(LightningDelay >= 0)
+        if(lightningTimer >= 0)
         {
-            
+            canAttack = true;
+
             //Physics2D.Raycast(LightningStartPos, Vector2.down * 10, Color.red);
-            LightningDelay -= Time.deltaTime;
+            lineRenderer.SetPosition(0, lightningStartPos);
+            lineRenderer.SetPosition(1, lightningEndPos);
+            lineRenderer.enabled = true;
+
+            lightningTimer -= Time.deltaTime;
         }
 
-        else if (LightningDelay <= 0)
+        else if (lightningTimer <= 0)
         {
+            if(lineRenderer.enabled)
+            {
+                lineRenderer.enabled = false;
+            }
+
+            if(canAttack)
+            {
+                
+                Instantiate(lightningStrike, lightningStartPos, Quaternion.Euler(90, 0, 0));
+                canAttack = false;
+            }
             timer -= Time.deltaTime;
-            Instantiate(lightningStrike, LightningStartPos, Quaternion.identity);
+
         }
-        
-           
+
+
 
         if (timer <= 0)
         {
             animator.SetTrigger("Idle");
+        }
+    }
+
+
+    void ActivateLightning()
+    {
+        if(lightningObject != null)
+        {
+            lightningObject.transform.position = lightningStartPos;
+            lightningObject.SetActive(true);
+
         }
     }
 
