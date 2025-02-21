e565f21ef0cb062b1d521c530c5b6e95ed97942f
GongMinGi <komki@naver.com>
2025-02-21 19:05:23 +0900

    json 기반 저장, 슬롯 구현중

---
+++ Assets/Scripts/gmg/DataManager.cs
@@ -0,0 +1,88 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using System.IO; //어딘가로 넣거나 가져올때 쓰임 input /output
+
+/* 저장 방법
+ *  1. 저장할 데이터가 존재
+ *  2. 데이털르 제이슨으로 변환
+ *  3. 제이슨을 외부에 저장
+ *  
+ * 불러오기
+ *  1. 외부에 저장할 제이슨을 가져옴
+ *  2. 제이슨을 데이터 형태로 변환
+ *  3. 불러온 데이터를 사용
+ */
+
+
+public class PlayerData
+{
+    // 이름, 레벨, 코인, 착용중인 무기
+    public string nowScene;
+    public string name;
+    public int level;
+    public int coin;
+    public int item; 
+}
+
+
+//데이터 관리 및 저장/불러오기 기능을 담당하는 Monobehaviour 클래스
+public class DataManager : MonoBehaviour
+{
+    //DataManager의 싱글톤 인스턴스 (프로젝트 내에서 하나만 존재하게 함)
+    public static DataManager Instance;
+    
+    // 현재 플레이어 데이터를 저장할 변수( playerData 타입의 인스턴스)
+    public PlayerData nowPlayer = new PlayerData();
+
+    public string path;                //저장파일의 경로를 저장할 변수
+    //string filename = "save";   //저장파일의 이름을 지정 (확장자 없이 단순 이름), 현재는 아래의 path변수에 save가 직접 들어가있다
+    public int nowSlot;         //파일 이름이 슬롯마다 같으려면, nowSlot 변수를 이용해서 현재 저장에 사용하는 슬롯 번호를 매겨준다.
+
+    // 스크립트 인스턴스가 활성화 될 때 호출
+    private void Awake()
+    {
+        //애플리케이션의 영구 데이터 저장 경로를 가져와 path 변수에 저장
+        //persistentDataPath는 플랫폼에 맞는 경로를 제공 ( 유니티의 경우 프로젝트 폴더)
+        path = Application.persistentDataPath + "/save"; // + "/"를 붙여야 jsonutility를 통해 파일이름을 설정할 수 있다.
+
+
+        //싱글톤 인스턴스가 아직 할당되지 않앗다면 현재 인스턴스 할당
+        if(Instance == null )
+        {
+            Instance = this;
+        }
+        //이미 다른 인스턴스가 존재한다면, 중복을 제거하기 위해 기존 인스턴스의 게임 오브젝트를 파괴
+        else if(Instance != this)
+        {
+            Destroy(Instance.gameObject);
+        }
+        DontDestroyOnLoad(Instance);
+    }
+
+
+    void Start()
+    {
+
+    }
+
+    //현재 플레이어의 정보를 제이슨으로 바꾸고 저장
+    public void SaveData()
+    {
+        string data = JsonUtility.ToJson(nowPlayer); // json은 string이기 때문에 문자열로 받을 수 있음 
+        File.WriteAllText(path + nowSlot.ToString(), data);   //저장하고자 하는 경로에 +를 붙여서 파일의 이름을 설정할 수 있다.
+        // + nowSlot을 햇기 때문에 이름이 같다고 하더라도 뒤에 0, 1, 2 가 붙기에 구분할 수 있다.
+    }
+
+    public void LoadData()
+    {
+        string data = File.ReadAllText(path + nowSlot.ToString()); // 경로와 불러올 파일의 이름을 매개로 데이터를 가져온다.
+        nowPlayer = JsonUtility.FromJson<PlayerData>(data);
+    }
+
+
+    public void DataClear()
+    {
+        nowSlot = -1; // 슬롯 번호는 -가 될 수 없으므로 -로 초기화
+        nowPlayer = new PlayerData(); //플레이어 정보 초기화
+    }
+}
---
+++ Assets/Scripts/gmg/SelectData.cs
@@ -0,0 +1,92 @@
+using JetBrains.Annotations;
+using System.IO;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SelectData : MonoBehaviour
+{
+    //비어있는 슬롯을 눌렀을 때 뜨는 창
+    public GameObject createViewPort;
+    public Text[] slotText;             //데이터를 저장할 때 슬롯의 문구가 바뀌어야 하기 때문에 슬롯의 텍스트를 변수로 가지고 있어야 함
+    public Text newPlayerName;           // 새로운 저장 데이터를 만들 때 입력받을 플레이어 이름
+
+    bool[] savefile = new bool[3];
+
+    void Start()
+    {
+        // 슬롯 별로 저장된 데이터가 존재하는지 판단
+        // 저장됬던 경로를 확인해야 함.
+        for(int i =0; i< 3; i++)
+        {
+            //0~2까지 돌면서 파일이 존재하는지 아닌지 bool값으로 반환한다.
+            if(File.Exists(DataManager.Instance.path + $"{i}"))
+            {
+                savefile[i] = true;
+                DataManager.Instance.nowSlot = i;   // 데이터가 존재한다면, 현재 슬롯에 해당 슬롯의 번호를 입력
+                DataManager.Instance.LoadData();    // 해당 슬롯에 저장된 데이터를 불러와 DataManager의 nowPlayer에 할당
+                slotText[i].text = DataManager.Instance.nowPlayer.name; // 불러온 데이터의 플레이어 이름을 슬롯의 텍스트에 적어놓는다.
+            }
+            else // 파일이 존재하지 않는다면,
+            {
+                slotText[i].text = "비어 있음";
+            }
+
+        }
+        DataManager.Instance.DataClear(); // 실제로 데이터를 반영하려는 것이 아닌 슬롯에 표시만 하려는 것이기 때문에, 전부 불러온 다음엔 초기화해준다.
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //슬롯에 들어갈 함수
+    //슬롯이 3개인데 어떻게 알맞게 불러오는가
+    //슬롯 버튼 클릭시 호출되는 메서드, 클릭된 슬롯의 번호(number)를 매개변수로 받음
+    public void Slot(int number)
+    {
+        DataManager.Instance.nowSlot = number; // 함수를 실행할 때 입력된 숫자가 나우슬롯에 들어감
+
+        if (savefile[number])
+        {
+            // 2. 저장된 데이터가 있을 때 => 불러오기 해서 게임씬으로 넘어감
+            DataManager.Instance.LoadData();
+            GoGame(); // 세이데이터가 있을 때만 게임 씬으로 바로 넘어간다.
+
+        }
+        else  // 1. 저장된 데이터가 없을 때
+        {
+            createNewPlayer(); // 새 플레이어 생성을 위한 UI 창을 활성화하는 메서드 (빈 슬롯 클릭 시 새 데이터 생성)
+
+        }
+
+
+    }
+
+    //빈 슬롯을 누르면 캐릭터를 새로 생성시킨다.
+    public void createNewPlayer()
+    {
+        createViewPort.gameObject.SetActive(true);  //플레이어 생성 창을 활성화하여 화면에 표시.
+    }
+
+
+    public void GoGame()
+    {
+        //게임 씬으로 이동하기 전에 입력을 받으면 이미 저장되어있던 경우 빈 값을 입력하는 것이나 마찬가지이기 때문에 이름이 없어지는 버그가 생긴다.
+        if (!savefile[DataManager.Instance.nowSlot]) // 현재 슬롯에 데이터가 저장되어 있지 않다면,
+        {
+            DataManager.Instance.nowPlayer.name = newPlayerName.text; //슬롯을 만들 때 입력했던 이름을 현재데이터의 이름에 기록한다.
+            DataManager.Instance.SaveData();                            // 새 데이터를 파일에 저장
+        }
+
+        SceneManager.LoadScene(1); // 인덱스 1에 해당하는 씬(게임 씬)으로 전환
+    }
+
+
+
+}
